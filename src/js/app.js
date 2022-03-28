94255d0a1e55fcac5df7a2e5927897a7b0bec662
Cerasus04 <cerasus1004@gmail.com>
2022-03-29 01:10:05 +0200

    fix(slider_section)

--- src/js/app.js
+++ src/js/app.js
@@ -6,8 +6,6 @@ import pageWidgetInit from './dev_vendors/dev_widget';
 // ------------------- imports###
 
 // ------------------  import components
-import { slider } from './components/slider';
-// import { menu } from './components/menu';
 
 // ------------------  import components###
 
@@ -31,7 +29,6 @@ const readyFunc = () => {
 
 const loadFunc = () => {
 	console.log('Вот же оно');
-	slider();
 };
 
 documentReady(() => {
@@ -42,15 +39,18 @@ pageLoad(() => {
 	loadFunc();
 });
 // open menu
+const page = document.querySelector('.page');
 const burger = document.querySelector('.header__burger_btn');
 const header = document.querySelector('.header');
 const closeBtn = document.querySelector('.header__close_btn');
 
 const openList = () => {
 	header.classList.add('header--open');
+	page.classList.add('hidden');
 };
 const closeList = () => {
 	header.classList.remove('header--open');
+	page.classList.remove('hidden');
 };
 burger.addEventListener('click', openList);
 closeBtn.addEventListener('click', closeList);
