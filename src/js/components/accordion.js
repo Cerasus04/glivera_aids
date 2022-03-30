43426c9d1619d21ca7343e31b87d38aa3403e979
Cerasus04 <cerasus1004@gmail.com>
2022-03-30 23:24:48 +0200

    fix(slider_section): js

--- src/js/components/accordion.js
+++ src/js/components/accordion.js
@@ -1,54 +1,57 @@
 const sliderAccordion = () => {
-	const accordion = document.querySelector('.accordion');
-	if (accordion) {
-		const sliderSection = accordion.classList.contains('slider_section');
+	let windowWidth = window.innerWidth;
+	document.querySelector('.slider_section__item').classList.add('active');
+	$(window).on('resize', () => {
+		windowWidth = window.innerWidth;
+	});
+	const slides = document.querySelectorAll('.controls_list__btn');
+	const sliderItem = document.querySelectorAll('.slider_section__item');
 
-		let openedTab;
+	// Скрипт для оживления слайдов
 
-		const closeTab = () => {
-			openedTab.classList.remove('slider_section__item--open');
-			openedTab = null;
-		};
+	slides.forEach((slide) => {
+		slide.addEventListener('click', (evt) => {
+			evt.preventDefault();
 
-		accordion.classList.add('accordion--js');
-		accordion.addEventListener('click', (e) => {
-			if (e.target.classList.contains('accordion__button')) {
-				if (sliderSection && openedTab && e.target.parentElement !== openedTab) {
-					closeTab();
-				}
-				e.target.parentElement.classList.toggle('slider_section__item--open');
-				openedTab = e.target.parentElement;
-			}
+			slides.forEach((item) => {
+				item.classList.remove('current');
+			});
+			evt.target.classList.add('current');
+			sliderItem.forEach((item) => {
+				item.classList.remove('active');
+			});
+			sliderItem[evt.target.id - 1].classList.add('active');
 		});
+	});
+
+	if (windowWidth > 1023) {
+		const mobileSlider = document.querySelector('.slider_section__item');
+		if (mobileSlider.classList.contains('active')) {
+			mobileSlider.classList.remove('active');
+		}
+		const accordion = document.querySelector('.accordion');
+		if (accordion) {
+			const sliderSection = accordion.classList.contains('slider_section');
+
+			let openedTab;
+
+			const closeTab = () => {
+				openedTab.classList.remove('slider_section__item--open');
+				openedTab = null;
+			};
+
+			accordion.classList.add('accordion--js');
+			accordion.addEventListener('click', (e) => {
+				if (e.target.classList.contains('accordion__button')) {
+					if (sliderSection && openedTab && e.target.parentElement !== openedTab) {
+						closeTab();
+					}
+					e.target.parentElement.classList.toggle('slider_section__item--open');
+					openedTab = e.target.parentElement;
+				}
+			});
+		}
 	}
 };
 
 export default sliderAccordion;
-
-// const sliderAccordion = () => {
-// 	const accordion = document.querySelector('.accordion');
-// 	if (accordion) {
-// 		const sliderSection = accordion.classList.contains('slider_section');
-
-// 		let openedTab;
-
-// 		const closeTab = () => {
-// 			openedTab.classList.remove('slider_section__item--open');
-// 			openedTab = null;
-// 		};
-
-// 		accordion.classList.add('accordion--js');
-// 		accordion.addEventListener('click', (e) => {
-// 			if (e.target.classList.contains('accordion__button')) {
-// 				if (sliderSection && openedTab && e.target.parentElement !== openedTab) {
-// 					closeTab();
-// 				}
-// 				e.target.parentElement.classList.toggle('slider_section__item--open');
-// 				openedTab = e.target.parentElement;
-// 			}
-// 		});
-// 	}
-// 	console.log('заходить в слайдер');
-// };
-
-// export default sliderAccordion;
