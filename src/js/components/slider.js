44b0ac1173bbe8802e0b7ea2aa6ddf4f191cb60f
Cerasus04 <cerasus1004@gmail.com>
2022-04-06 16:57:49 +0200

    slider mobile

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -3,6 +3,17 @@ import 'swiper/css';
 
 const slider = () => {
 	const swiper = new Swiper('.swiper', {
+		slidesPerView: 1,
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+		breakpoints: {
+			1023: {
+				slidesPerView: 1,
+				spaceBetween: 1,
+			},
+		},
 	});
 };
 export default slider;
