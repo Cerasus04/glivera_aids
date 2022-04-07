c444fec1718390cfe03c39afe0e52aeedf9b2096
Cerasus04 <cerasus1004@gmail.com>
2022-04-07 08:08:07 +0200

    add resize in js

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,45 +1,33 @@
 import Swiper, { Pagination } from 'swiper';
 import 'swiper/css/bundle';
 
-// const slider = () => {
-// 	const $sectionSlider = document.querySelector('.sliderList');
-// 	const $sectionItem = $sectionSlider.querySelectorAll('.sliderItem');
-// 	$sectionSlider.classList.add('swiper-wrapper');
-// 	for (let i = 0; i < $sectionItem.length; i += 1) {
-// 		$sectionItem[i].classList.add('swiper-slide');
-// 	}
-// 	$sectionItem.classList.add('swiper-slide');
-// 	let windowWidth = window.innerWidth;
-// 	const swiper = new Swiper('.swiper', {
-// 		modules: [Pagination],
-// 		pagination: {
-// 			el: '.swiper-pagination',
-// 			clickable: true,
-// 		},
-// 	});
-// 	$(window).on('resize', () => {
-// 		windowWidth = window.innerWidth;
-// 	});
-
-// 	if (windowWidth > 1023) {
-// 		console.log('зашел проверить');
-// 		if ($sectionSlider.classList.contains('swiper-wrapper')) {
-// 			$sectionSlider.classList.remove('swiper-wrapper');
-// 			for (let i = 0; i < $sectionItem.length; i += 1) {
-// 				$sectionItem[i].classList.remove('swiper-slide');
-// 			}
-// 		}
-// 	}
-// };
-// export default slider;
-
 const slider = () => {
-	const swiper = new Swiper('.swiper', {
-		modules: [Pagination],
-		pagination: {
-			el: '.swiper-pagination',
-			clickable: true,
-		},
+	const $slider = document.querySelector('.slider');
+	const $sectionSlider = document.querySelector('.sliderList');
+	const $sectionItem = $sectionSlider.querySelectorAll('.sliderItem');
+
+	let windowWidth = window.innerWidth;
+	$(window).on('resize', () => {
+		windowWidth = window.innerWidth;
 	});
+
+	if (windowWidth < 1023) {
+		console.log('зашел проверить');
+		$slider.classList.add('sliderJs');
+		if ($slider.classList.contains('sliderJs')) {
+			$slider.classList.add('swiper');
+			$sectionSlider.classList.add('swiper-wrapper');
+			for (let i = 0; i < $sectionItem.length; i += 1) {
+				$sectionItem[i].classList.add('swiper-slide');
+			}
+			const swiper = new Swiper('.swiper', {
+				modules: [Pagination],
+				pagination: {
+					el: '.swiper-pagination',
+					clickable: true,
+				},
+			});
+		}
+	}
 };
 export default slider;
