9dd9ad18edb56cda836ac2cbaebc3fdefa0f368c
Cerasus04 <cerasus1004@gmail.com>
2022-04-07 07:53:21 +0200

    fix style

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,6 +1,38 @@
 import Swiper, { Pagination } from 'swiper';
 import 'swiper/css/bundle';
 
+// const slider = () => {
+// 	const $sectionSlider = document.querySelector('.sliderList');
+// 	const $sectionItem = $sectionSlider.querySelectorAll('.sliderItem');
+// 	$sectionSlider.classList.add('swiper-wrapper');
+// 	for (let i = 0; i < $sectionItem.length; i += 1) {
+// 		$sectionItem[i].classList.add('swiper-slide');
+// 	}
+// 	$sectionItem.classList.add('swiper-slide');
+// 	let windowWidth = window.innerWidth;
+// 	const swiper = new Swiper('.swiper', {
+// 		modules: [Pagination],
+// 		pagination: {
+// 			el: '.swiper-pagination',
+// 			clickable: true,
+// 		},
+// 	});
+// 	$(window).on('resize', () => {
+// 		windowWidth = window.innerWidth;
+// 	});
+
+// 	if (windowWidth > 1023) {
+// 		console.log('зашел проверить');
+// 		if ($sectionSlider.classList.contains('swiper-wrapper')) {
+// 			$sectionSlider.classList.remove('swiper-wrapper');
+// 			for (let i = 0; i < $sectionItem.length; i += 1) {
+// 				$sectionItem[i].classList.remove('swiper-slide');
+// 			}
+// 		}
+// 	}
+// };
+// export default slider;
+
 const slider = () => {
 	const swiper = new Swiper('.swiper', {
 		modules: [Pagination],
