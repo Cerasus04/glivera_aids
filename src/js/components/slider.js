d2be6a81c3b9dacdaa140842a0c26897a62a2d47
Cerasus04 <cerasus1004@gmail.com>
2022-04-20 15:18:13 +0200

    slider resize

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -7,27 +7,36 @@ const slider = () => {
 	const $sectionItem = $sectionSlider.querySelectorAll('.sliderItem');
 
 	let windowWidth = window.innerWidth;
+	let swiper = null;
 	$(window).on('resize', () => {
 		windowWidth = window.innerWidth;
-	});
-
-	if (windowWidth < 1023) {
-		$slider.classList.add('sliderJs');
-		if ($slider.classList.contains('sliderJs')) {
-			$slider.classList.add('swiper');
-			$sectionSlider.classList.add('swiper-wrapper');
-			for (let i = 0; i < $sectionItem.length; i += 1) {
-				$sectionItem[i].classList.add('swiper-slide');
+		if (windowWidth < 1023) {
+			$slider.classList.add('sliderJs');
+			if ($slider.classList.contains('sliderJs')) {
+				$slider.classList.add('swiper');
+				$sectionSlider.classList.add('swiper-wrapper');
+				for (let i = 0; i < $sectionItem.length; i += 1) {
+					$sectionItem[i].classList.add('swiper-slide');
+				}
+				if (!swiper) {
+					console.log('init');
+					swiper = new Swiper('.swiper', {
+						modules: [Pagination],
+						pagination: {
+							el: '.swiper-pagination',
+							clickable: true,
+						},
+						loop: true,
+					});
+				}
 			}
-			const swiper = new Swiper('.swiper', {
-				modules: [Pagination],
-				pagination: {
-					el: '.swiper-pagination',
-					clickable: true,
-				},
-				loop: true,
-			});
+		} else {
+			if (swiper) {
+				console.log('destroy');
+				swiper.destroy();
+				swiper = null;
+			}
 		}
-	}
+	});
 };
 export default slider;
